33dc5d85bd8ab7e67cd62958e650c5fce801ff39
taichi imai <imai0916taiti@gmail.com>
2017-11-04 06:16:47 +0000

    heroku

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,44 @@
+class ItemsController < ApplicationController
+  before_action :require_user_logged_in
+  
+  def show
+   @item = Item.find(params[:id])
+   @want_users = @item.want_users
+   @have_users = @item.have_users
+  end
+  
+  def new
+    @items = []
+
+    @keyword = params[:keyword]
+    if @keyword
+      results = RakutenWebService::Ichiba::Item.search({
+        keyword: @keyword,
+        imageFlag: 1,
+        hits: 20,
+      })
+
+     results.each do |result|
+        # 扱い易いように Item としてインスタンスを作成する（保存はしない）
+        item = Item.find_or_initialize_by(read(result))
+        @items << item
+      end
+    end
+  end
+  
+  private
+  
+  def read(result)
+    code = result['itemCode']
+    name = result['itemName']
+    url = result['itemUrl']
+    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+ 
+    return {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end
+end
