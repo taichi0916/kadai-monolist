b48dff5e05962c545d0c01446dff3338d8b5b40c
taichi imai <imai0916taiti@gmail.com>
2017-11-05 06:47:35 +0000

    ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,4 +1,4 @@
-class RankingController < ApplicationController
+class RankingsController < ApplicationController
   def want
     @ranking_counts = Want.ranking
     @items = Item.find(@ranking_counts.keys)
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
   
-  get 'ranking/want', to: 'rankings#want'
+  get 'rankings/want', to: 'rankings#want'
   
   resources :items, only: [:show, :new] 
   resources :ownerships, only: [:create, :destroy]
